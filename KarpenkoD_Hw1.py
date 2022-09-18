a182389258386a6e0a821a29e5654acddd15e0bd
1Denys <49844753+1Denys@users.noreply.github.com>
2022-09-18 12:08:00 +0300

    Add files via upload

---
+++ KarpenkoD_Hw1.py
@@ -0,0 +1,27 @@
+# Exercise 1
+print('Hello world')
+
+# Exercise 2
+print('Hello Python')
+print('My name is Denys')
+print('I want to learn Python:)')
+
+# Exercise 3
+length = float(input('Enter length of the rectangle: '))
+width = float(input('Enter width of the rectangle: '))
+print('Area of the rectangle equals:', length * width)
+
+# Exercise 4
+num_1 = float(input('Enter the first number: '))
+num_2 = float(input('Enter the second number: '))
+print('Sum equals:', num_1 + num_2)
+print('Product equals:', num_1 * num_2)
+print('Difference equals:', num_1 - num_2)
+print('Quotient equals:', num_1 / num_2)
+
+# Exercise 5
+radius = float(input('Enter the radius of a circle: '))
+Pi = 3.14159
+print('Diameter of circle equals:', 2 * radius)
+print('Circumference of circle equals:', 2 * Pi * radius)
+print('Area of circle equals:', Pi * (radius ** 2))
